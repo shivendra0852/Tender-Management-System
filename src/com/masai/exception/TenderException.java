8cc9138d2413e752c79e76d845114bf2cb05b66b
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2022-11-11 19:26:02 +0530

    tendy day-2 committed

---
+++ src/com/masai/exception/TenderException.java
@@ -0,0 +1,18 @@
+package com.masai.exception;
+
+public class TenderException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TenderException() {
+		super();
+	}
+	
+	public TenderException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/com/masai/exception/VendorException.java
@@ -0,0 +1,21 @@
+package com.masai.exception;
+
+public class VendorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VendorException() {
+		super();
+	}
+
+
+	public VendorException(String message) {
+		super(message);
+	}
+
+	
+	
+}
